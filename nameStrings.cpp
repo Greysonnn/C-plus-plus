157b38a240f406892414cd7b7a2a5448c2a5244b
Greysonnn <25331809+Greysonnn@users.noreply.github.com>
2021-12-10 00:38:40 -0700

    C++ strings

---
+++ nameStrings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   string firstName;
+   string lastName;
+
+   cin >> firstName;
+   cin >> lastName;
+   
+   cout << lastName << ", " << firstName << endl;
+
+   return 0;
+}
