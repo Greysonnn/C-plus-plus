88681f876070d3abdecd565b230082ffe0e5b972
Greysonnn <25331809+Greysonnn@users.noreply.github.com>
2021-12-31 01:27:23 -0700

    Create origLetter.cpp

---
+++ origLetter.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   char origLetter;
+
+   cin >> origLetter;
+
+   /* Your solution goes here  */
+   switch(origLetter){
+      
+      case 'a':
+      case 'A':
+      cout << "Alpha" << endl;
+      break;
+      
+      case 'b':
+      case 'B':
+      cout << "Beta" << endl;
+      break;
+      
+      default:
+      cout << "Unknown" << endl;
+      break;
+   }
+
+   return 0;
+}
